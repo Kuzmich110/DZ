7143f9f30970a0f4c15f6f16e67e753dc21cc767
Kuzmich110 <71772259+Kuzmich110@users.noreply.github.com>
2020-10-10 14:48:29 +0300

    Create oop.php

---
+++ OOP/oop.php
@@ -0,0 +1,34 @@
+<?
+header('Content-Type: text/html; charset=utf-8');
+class Person{
+  private $name;
+  private $lastname;
+  private $age;
+  private $mother;
+  private $father;
+  
+  function __construct($name,$lastname,$age,$mother=null,$father=null){
+    $this->name = $name;
+    $this->lastname = $lastname;
+    $this->age = $age;
+    $this->mother = $mother;
+    $this->father = $father;
+  }
+  public function getName(){return $this->name;}
+	public function getLastname(){return $this->lastname;}
+	public function getAge(){return $this->age;}
+	public function getMother(){return $this->mother;}
+	public function getFather(){return $this->father;}
+	public function getInfo(){
+	  return "
+	    Меня зовут: ".$this->name."<br>
+	    Мою маму зовут: ".$this->mother->name."<br>
+	    Моего папу зовут: ".$this->getFather()->getName();
+	}
+}
+
+$oleg = new person("Олег","Петров",41);
+$olga = new person("Ольга","Петрова",41);
+$igor = new person("Игорь","Петров",12,$olga,$oleg);
+echo $igor->getInfo();
+?>
